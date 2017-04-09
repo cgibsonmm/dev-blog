7bfe697bd5012a0bc1ff1491e63b657762869b70
Corey Gibson <cgibsonmm@gmail.com>
2017-04-09 14:12:30 -0400

    finished part 1 of tutorial

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass'
+gem 'ckeditor'
 
 
 group :development, :test do
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update, :show, :delete]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post created successfully!'
+      redirect_to @post
+    else
+      flash[:alert] = 'Error creating post!'
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'Post updated successfully!'
+      redirect_to @post
+    else
+      flash[:alert] = 'Error updating post!'
+      rennder 'edit'
+    end
+  end
+
+  def show; end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = 'Successfully deleted post!'
+      redirect_to posts_path
+    else
+      flash[:alert] = 'Error deleting post!'
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+end
